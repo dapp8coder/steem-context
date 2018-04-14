c0083f23b5e202e7deda7ff0860287c7aa770b0d
Diego Hernandes <diego@hernandev.com>
2018-04-14 09:04:17 -0300

    locales (stub, not yet translated)

---
+++ src/_locales/en/messages.js
@@ -0,0 +1,15 @@
+/**
+ * @see {@link https://developer.chrome.com/extensions/i18n}
+ */
+module.exports = {
+  extName: { message: 'SteemContext' },
+  extDescription: { message: 'Easily switch between Steem frontend applications.' },
+
+  background: { message: 'background!' },
+  content: { message: 'content-script!' },
+  devtools: { message: 'devtools' },
+  fromCallback: { message: 'hello from callback' },
+  options: { message: 'options' },
+  popup: { message: 'New tab' },
+  tab: { message: 'tab' }
+}
---
+++ src/_locales/pt_BR/messages.js
@@ -0,0 +1,15 @@
+/**
+ * @see {@link https://developer.chrome.com/extensions/i18n}
+ */
+module.exports = {
+  extName: { message: 'SteemContext' },
+  extDescription: { message: 'Alterne facilmente entre as diversas aplicações do Steem.' },
+
+  background: { message: 'background!' },
+  content: { message: 'content-script!' },
+  devtools: { message: 'devtools' },
+  fromCallback: { message: 'Olá de callback' },
+  options: { message: 'opções' },
+  popup: { message: 'Nova aba' },
+  tab: { message: 'tab' }
+}
