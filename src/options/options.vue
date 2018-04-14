6e0b7715226d4856bf59f43808fdc69ecbdcf70e
Diego Hernandes <diego@hernandev.com>
2018-04-14 09:06:01 -0300

    options componet

---
+++ src/options/options.vue
@@ -0,0 +1,14 @@
+
+<script>
+  import resources from './resources'
+
+  // const __ = chrome.i18n.getMessage
+
+  export default {
+    mixins: [ resources ],
+    methods: { }
+  }
+</script>
+
+<template lang="pug" src="./options.pug"></template>
+<style src="./options.scss" lang="scss"></style>
