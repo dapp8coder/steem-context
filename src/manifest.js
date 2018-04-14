2b0b72635fde1d91106e8bf39d07ee2c4d7b5900
Diego Hernandes <diego@hernandev.com>
2018-04-14 09:09:46 -0300

    bump version for release

--- src/manifest.js
+++ src/manifest.js
@@ -5,7 +5,7 @@ module.exports = {
   name: '__MSG_extName__', // Vue Extension
   description: '__MSG_extDescription__', // Vue.js Webpack Chrome Extension Template
   author: 'Diego Hernandes <diego@hernandev.com>',
-  version: '1.0.0',
+  version: '1.0.1',
   icons: {
     '16': 'icons/16.png',
     '128': 'icons/128.png'
