f26c5f13e5f6f2d2ceb81ddc7696cfd194315b55
Diego Hernandes <diego@hernandev.com>
2018-04-14 09:05:11 -0300

    url utils

---
+++ src/background/url.js
@@ -0,0 +1,32 @@
+// imports.
+// import { get, mapValues } from 'lodash'
+
+/**
+ * Given a url schema, generate a chrome URL pattern for matching URLs.
+ *
+ * @param url
+ *
+ * @return String
+ */
+export const generatePattern = (url) => {
+  return url
+    .replace('https://', '*://')
+    .replace('http://', '*://')
+    .replace('{category}', '*')
+    .replace('{username}', '*')
+    .replace('{permlink}', '*')
+}
+
+/**
+ * Given a url schema, generate a corresponding route.
+ *
+ * @param url
+ *
+ * @return String
+ */
+export const generateRoute = (url) => {
+  return url
+    .replace('{category}/', '(:category/)')
+    .replace('{username}', ':username')
+    .replace('{permlink}', ':permlink')
+}
